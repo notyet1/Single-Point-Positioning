b455d852780ee439c918a1721c0b7e4c82f22fc6
Zzzen <66628017+notyet1@users.noreply.github.com>
2024-12-19 16:38:54 +0800

    Update test.cpp

--- Single-Point Positioning/test.cpp
+++ Single-Point Positioning/test.cpp
@@ -2,9 +2,9 @@
 int main() {
 	DATA data;
 	string s;
+	data.FileRead_GMO("TWTF00TWN_R_20242090000_01D_30S_MO.rnx");
 	data.FileRead_GMN("BRDM00DLR_S_20242090000_01D_MN.rnx");
 	std::getline(std::cin, s);
 	data.SinglePointPosition(s);
-	system("pause");
 	return 0;
 }
